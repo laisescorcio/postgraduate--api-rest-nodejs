f4cbcbd5cabf52ed47cc697ea56326d42280420b
Laís Escorcio <61030205+laisescorcio@users.noreply.github.com>
2025-03-18 07:51:24 -0300

    feat: to search data

--- src/server.ts
+++ src/server.ts
@@ -7,17 +7,17 @@ const app = fastify()
 // GET, POST, PUT, PATCH, DELETE
 
 app.get('/hello', async () => {
-  const transaction = await knex('transactions')
-    .insert({
-      id: crypto.randomUUID(),
-      title: 'Transação de teste',
-      amount: 1000,
-    })
-    .returning('*') // to return all inserted datas
-
   // const transaction = await knex('transactions')
-  // .where('amount', 1000)
-  // .select('*')
+  //   .insert({
+  //     id: crypto.randomUUID(),
+  //     title: 'Transação de teste',
+  //     amount: 1000,
+  //   })
+  //   .returning('*') // to return all inserted datas
+
+  const transaction = await knex('transactions')
+    .where('amount', 1000)
+    .select('*')
 
   return transaction
 })

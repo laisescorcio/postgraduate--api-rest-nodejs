639be54a9b06ab64e066e0dc9e96e8d8fa8103d0
Laís Escorcio <61030205+laisescorcio@users.noreply.github.com>
2025-03-18 08:00:04 -0300

    feat: environmental variables .env

--- src/database.ts
+++ src/database.ts
@@ -1,10 +1,15 @@
+import 'dotenv/config'
 import { knex as setupKnex, Knex } from 'knex'
+
+if (!process.env.DATABASE_URL) {
+  throw new Error('DATABASE_URL env not found')
+}
 
 // to export only the configurations to knexfile.ts
 export const config: Knex.Config = {
   client: 'sqlite',
   connection: {
-    filename: './db/app.db', // file name of where data base will be create
+    filename: process.env.DATABASE_URL,
   },
   useNullAsDefault: true, // the default value is null (require of sqlite3),
   migrations: {
